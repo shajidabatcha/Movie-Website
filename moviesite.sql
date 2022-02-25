fb3dd55e96c95647950721aebfa9e0e53eb961bd
shajida <90963334+shajidabatcha@users.noreply.github.com>
2022-02-25 22:59:58 +0530

    Create moviesite.sql

---
+++ moviesite.sql
@@ -0,0 +1,3 @@
+
+CREATE TABLE `moviesite`.`register` ( `uname1` VARCHAR(10) NOT NULL , `email` VARCHAR(10) NOT NULL , `upswd1` VARCHAR(10) NOT NULL , `upswd2` VARCHAR(10) NOT NULL ) ENGINE = InnoDB;
+CREATE TABLE `moviesite`.`signin` ( `email` VARCHAR(10) NOT NULL , `upswd1` VARCHAR(10) NOT NULL ) ENGINE = InnoDB;
